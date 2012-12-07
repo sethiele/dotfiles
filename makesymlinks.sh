e402f561545e78ceafc56cf1fafcf43eca82e723
Sebastian Thiele <sebastian.thiele@infopark.de>
2012-12-07 11:16:25 +0100

    new install file

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,31 +1,6 @@
-#!/bin/bash
-############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
+#!/usr/bin/env ruby
 
-########## Variables
-
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
-
-##########
-
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
-
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
-done
+Dir["dot/*"].each do |file|
+  link_target = File.join(ENV['HOME'], ".#{File.basename(file)}")
+  system "ln -v -n -f -s #{File.expand_path(file)} #{link_target}"
+end
