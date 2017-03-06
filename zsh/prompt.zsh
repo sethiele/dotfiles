6cbde23a5663bf4a6908b2415879c94c3200847b
Sebastian Thiele <sebastian+git@thiele.me>
2017-03-06 09:27:53 +0100

    Path fix

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -99,9 +99,9 @@ function current_pwd {
 }
 
 PROMPT='
-${PR_GREEN}%n%{$reset_color%} %{$FG[239]%}at%{$reset_color%} ${PR_BOLD_BLUE}$(box_name)%{$reset_color%} %{$FG[239]%}in%{$reset_color%} ${PR_BOLD_YELLOW}$(current_pwd)%{$reset_color%} $(git_prompt_string)
+${PR_GREEN}%n%{$reset_color%} at ${PR_BOLD_BLUE}$(box_name)%{$reset_color%} in ${PR_BOLD_YELLOW}$(current_pwd)%{$reset_color%} $(git_prompt_string)
 $(prompt_char) '
 
 export SPROMPT="Correct $fg[red]%R$reset_color to $fg[green]%r$reset_color [(y)es (n)o (a)bort (e)dit]? "
 
-RPROMPT='${PR_GREEN}$(virtualenv_info)%{$reset_color%} ${PR_RED}${ruby_version}%{$reset_color%} ⌚${PR_BLUE}%*%{$reset_color%}'
+RPROMPT='${PR_GREEN}$(virtualenv_info)%{$reset_color%} ${PR_RED}${ruby_version}%{$reset_color%} ⌚ ${PR_BLUE}%*%{$reset_color%}'
